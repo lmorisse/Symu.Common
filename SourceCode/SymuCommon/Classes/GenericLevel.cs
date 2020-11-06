f6c635dd1f08e4563948760fe50072f0e1d3a0a0
lmorisse <laurent.morisseau@gmail.com>
2020-11-06 10:49:39 +0100

    Refactoring Symu.Common from Symu.org

---
+++ SourceCode/SymuCommon/Classes/GenericLevel.cs
@@ -0,0 +1,22 @@
+﻿#region Licence
+
+// Description: SymuBiz - Symu
+// Website: https://symu.org
+// Copyright: (c) 2020 laurent morisseau
+// License : the program is distributed under the terms of the GNU General Public License
+
+#endregion
+
+namespace Symu.Common.Classes
+{
+    public enum GenericLevel
+    {
+        None,
+        VeryLow,
+        Low,
+        Medium,
+        High,
+        VeryHigh,
+        Complete
+    }
+}
